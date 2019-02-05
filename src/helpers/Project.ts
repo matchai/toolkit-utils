c83491056d2076c603e4ca2892cf2c235df71cde
Matan Kushner <hello@matchai.me>
2019-02-04 20:59:53 -0500

    Add project util to get avilable scripts

--- src/helpers/Project.ts
+++ src/helpers/Project.ts
@@ -1,5 +1,6 @@
 import path from 'path';
-import fs, {readJSONSync} from 'fs-extra';
+import fs from 'fs-extra';
+import globby from 'globby';
 import { getToolkitRoot, getProjectPackage } from './project-util';
 
 export default class Project {
@@ -8,21 +9,66 @@ export default class Project {
   projectPkg: { [key: string]: any };
 
   constructor({
-    moduleRoot = getToolkitRoot(),
+    toolkitRoot = getToolkitRoot(),
     cwd
   }: {
-    moduleRoot?: string,
+    toolkitRoot?: string,
     cwd?: string
   } = {}) {
     try {
-      const toolkitPackage = fs.readJSONSync(path.join(moduleRoot, "package.json"));
-      const {pkg: projectPkg, root: projectRoot } = getProjectPackage(moduleRoot, toolkitPackage);
+      const toolkitPackage = fs.readJSONSync(path.join(toolkitRoot, "package.json"));
+      const {pkg: projectPkg, root: projectRoot } = getProjectPackage(toolkitRoot, toolkitPackage);
       this.projectName = projectPkg.name;
       this.projectRoot = projectRoot;
       this.projectPkg = projectPkg;
+
+      console.log(this.availableScripts)
+
+      // if (debug) {
+      //   this.logger.warn("Debug mode is on");
+      // }
     } catch (e) {
       console.error(e, "Cannot initialize project.");
       process.exit(1);
     }
+  }
+
+  /**
+   * List of scripts available in this toolkit
+   * @readonly
+   * @type {Array.<string>}
+   */
+  get availableScripts(): Array<string> {
+    // `globby` returns a list of unix style paths
+    // We normalize it and script out paths and file extensions
+    return globby
+    .sync(path.join(this.scriptsDir, "!(*.d.ts|*.map)"))
+    .map(path.normalize)
+    .map(script => 
+      script
+        .replace(this.scriptsDir, "")
+        .replace(/^[/\\]/, "")
+        .replace(/\.(js|ts)$/, "")
+    )
+    .filter(Boolean)
+    .sort();
+  }
+
+  /**
+   * Path of the scripts dir
+   * @readonly
+   * @type {string}
+   */
+  get scriptsDir() {
+    return path.join(this.srcDir, "scripts");
+  }
+
+  /**
+   * Path of the src dir
+   * @readonly
+   * @type {string}
+   */
+  get srcDir() {
+    return path.dirname(require!.main!.filename);
   }
 }
